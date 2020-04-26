f73690bfe31de208563f431da00883efba4c0e2c
nyinyizin1 <nyinyizin298@gmail.com>
2020-04-26 17:18:16 +0630

    competing assignment

---
+++ src/main/java/twonyizin/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package twonyizin.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Ingredient ingredient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
